273fa5140c0b0c7ba81be94e00a8e114992f46c3
tamalesimon <tamalesp@gmail.com>
2020-06-01 16:17:57 +0300

    Updates

---
+++ precache-manifest.a2b3c21e36eea622d798226bfac7366f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27503f376df039e0dc892779f6a75564",
+    "url": "/covid19uganda/index.html"
+  },
+  {
+    "revision": "0e87563abef6430bcc2c",
+    "url": "/covid19uganda/static/css/main.f693c9f5.chunk.css"
+  },
+  {
+    "revision": "c16d6c4899fd22dff4c3",
+    "url": "/covid19uganda/static/js/2.00dcaafc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid19uganda/static/js/2.00dcaafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e87563abef6430bcc2c",
+    "url": "/covid19uganda/static/js/main.a8b3654a.chunk.js"
+  },
+  {
+    "revision": "71e5cd0ecc3deaba53a8",
+    "url": "/covid19uganda/static/js/runtime-main.0ad2e172.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export {default as Cards} from './Cards/Cards.component';
-//export {default as Chart} from './Chart/Chart.component';
